499f19870b5746e774df911560fceb7c3540f2b9
seran261 <seran261@gmail.com>
2026-01-12 12:32:31 +0530

    Update telegram_config.py

--- telegram_config.py
+++ telegram_config.py
@@ -2,11 +2,23 @@
 
 import os
 
+# =========================
+# TELEGRAM TOGGLE
+# =========================
 TELEGRAM_ENABLED = True
 
+# =========================
+# TELEGRAM CREDS
+# =========================
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-# Emojis / style
+# =========================
+# MESSAGE STYLE
+# =========================
 SHOW_CONFIDENCE_BAR = True
 SHOW_TP_LADDER = True
+
+# Emojis
+BUY_EMOJI = "🟢🚀"
+SELL_EMOJI = "🔴📉"
