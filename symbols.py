bfad9a793da7b28ee402dfb297d447ed9c49619e
seran261 <seran261@gmail.com>
2026-01-12 12:29:54 +0530

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -1,20 +1,10 @@
 # symbols.py
 
-# =========================
-# NSE / BSE STOCK UNIVERSE
-# =========================
-# Keep 6–10 stocks for Alpha Vantage free tier
-
 STOCKS = {
-    "RELIANCE": "RELIANCE.BSE",
-    "TCS": "TCS.BSE",
-    "INFY": "INFY.BSE",
-    "HDFCBANK": "HDFCBANK.BSE",
-    "ICICIBANK": "ICICIBANK.BSE",
-    "SBIN": "SBIN.BSE",
-
-    # Optional (enable later)
-    # "ITC": "ITC.BSE",
-    # "LT": "LT.BSE",
-    # "AXISBANK": "AXISBANK.BSE",
+    "RELIANCE": "RELIANCE.NS",
+    "TCS": "TCS.NS",
+    "INFY": "INFY.NS",
+    "HDFCBANK": "HDFCBANK.NS",
+    "ICICIBANK": "ICICIBANK.NS",
+    "SBIN": "SBIN.NS",
 }
