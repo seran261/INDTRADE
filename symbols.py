2446be9b1bbc51ffba7ea034e25a58ce133f9df3
seran261 <seran261@gmail.com>
2026-01-12 12:31:31 +0530

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -7,4 +7,9 @@ STOCKS = {
     "HDFCBANK": "HDFCBANK.NS",
     "ICICIBANK": "ICICIBANK.NS",
     "SBIN": "SBIN.NS",
+
+    # Optional (add later)
+    # "ITC": "ITC.NS",
+    # "LT": "LT.NS",
+    # "AXISBANK": "AXISBANK.NS",
 }
